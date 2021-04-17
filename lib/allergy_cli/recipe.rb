1677440c5ba878dce04bd8c493ea6d77073c95f7
Milan <milanvpage@gmail.com>
2021-04-17 08:51:43 -0700

    created an initliaze method that takes in a array of recipes(recipe_list)  and iterates through each key/value pair

--- lib/allergy_cli/recipe.rb
+++ lib/allergy_cli/recipe.rb
@@ -2,4 +2,10 @@ class Recipe
     attr_accessor :title, :href, :ingredients
 
     @@all = []
+
+    def initialize(recipe_list)
+        recipe_list.each do |key, value|
+            self.send("#{key}=", value) if self.respond_to?("#{key}=")
+        end
+    end
 end
