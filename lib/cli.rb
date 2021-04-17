573acd290e33db06a43a2e0396d6c9b66440db4d
Milan <milanvpage@gmail.com>
2021-04-17 01:01:52 -0700

    created goodbye method inside cli.rb

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,28 +1,28 @@
 class CLI 
     #binding.pry
-        def start
-           puts "Welcome! I hope we can find you a recipe you love that matches your needs! Enter ingredients you would like to see in a Recipe and/or a Recipe to fit your Allergy needs?".colorize(:color => :light_blue)
+    def start
+       puts "Welcome! I hope we can find you a recipe you love that matches your needs! Enter ingredients you would like to see in a Recipe and/or a Recipe to fit your Allergy needs?".colorize(:color => :light_blue)
             
-            input = user_input
-            greet(input)
-        end
+        input = user_input
+        greet(input)
+    end
 
-        def user_input
-            gets.strip 
-          end
+    def user_input
+        gets.strip 
+    end
 
-        def greet(intolerance)
-          API.get_data(intolerance)
-          if Recipe.all.length != 0
-            puts "Lets see what Yummy dishes we can find with #{intolerance}! Enter y to see list, enter exit to exit".colorize(:color => :green)
-            menu
-          else
-            puts "Invalid entry lets bring you back to the start!"
-            start
-          end  
-       end  
+    def greet(intolerance)
+      API.get_data(intolerance)
+      if Recipe.all.length != 0
+        puts "Lets see what Yummy dishes we can find with #{intolerance}! Enter y to see list, enter exit to exit".colorize(:color => :green)
+        menu
+      else
+        puts "Invalid entry lets bring you back to the start!"
+        start
+      end  
+    end  
 
-       def menu
+    def menu
         #enter allergy and print out list of allergy recipe matches
         #enter exit want them to exit
         #enter invalid message 
@@ -37,4 +37,10 @@ class CLI
             invalid
         end
     end
+
+    def goodbye
+        puts "Hope we could find you a recipe to fill your tummy with Love!".colorize(:color => :light_blue)
+    end
+    
+
 end
