af4f27b5973fe3ccc7ba8279c02ef8751250b1cf
Milan <milanvpage@gmail.com>
2021-04-17 00:58:20 -0700

    created menu method inside cli.rb

--- lib/cli.rb
+++ lib/cli.rb
@@ -11,14 +11,30 @@ class CLI
             gets.strip 
           end
 
-          def greet(intolerance)
-            API.get_data(intolerance)
-            if Recipe.all.length != 0
-              puts "Lets see what Yummy dishes we can find with #{intolerance}! Enter y to see list, enter exit to exit".colorize(:color => :green)
-              menu
-            else
-              puts "Invalid entry lets bring you back to the start!"
-              start
-            end
+        def greet(intolerance)
+          API.get_data(intolerance)
+          if Recipe.all.length != 0
+            puts "Lets see what Yummy dishes we can find with #{intolerance}! Enter y to see list, enter exit to exit".colorize(:color => :green)
+            menu
+          else
+            puts "Invalid entry lets bring you back to the start!"
+            start
           end  
+       end  
+
+       def menu
+        #enter allergy and print out list of allergy recipe matches
+        #enter exit want them to exit
+        #enter invalid message 
+        input = user_input
+        if input == "y"
+            print_recipes
+            
+        elsif input == "exit"
+            goodbye
+        else
+            #invalid message and let them keep choosing
+            invalid
+        end
+    end
 end
