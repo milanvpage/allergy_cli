cca5dfa97817617681f1c07b5b00a45b448024ce
Milan <milanvpage@gmail.com>
2021-04-17 00:48:09 -0700

    created api.rb inside lib folder

---
+++ lib/allergy_cli/api.rb
@@ -0,0 +1,13 @@
+class API
+
+    def self.get_data(input)
+        response = RestClient.get("http://www.recipepuppy.com/api/?q=#{input}")
+        recipe_list = JSON.parse(response)["results"]
+        #attr_accessor :title, :url, :ingredients
+         recipe_list.each do |recipe|
+             #binding.pry
+            Recipe.new(recipe)
+         end
+ #binding.pry
+     end
+ end
